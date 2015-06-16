09f58f1962f2f9afa68e148bacd81e39af973e59
Darren Schnare <=>
2015-06-15 21:33:42 -0400

    Convert tabs to spaces in package.js template

--- templates/meteor/package.mustache.js
+++ templates/meteor/package.mustache.js
@@ -1,16 +1,16 @@
 Package.describe({
-	name: '{{#meteorUser}}{{.}}:{{/meteorUser}}{{destPkgPrefix}}{{pkg.name}}',
+  name: '{{#meteorUser}}{{.}}:{{/meteorUser}}{{destPkgPrefix}}{{pkg.name}}',
   summary: 'Bootstrap: {{pkg.name}}',
   version: '{{bootstrap.version}}'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('1.1.0.2');
-	api.use(['jquery', 'less'], 'client');
-	{{#pkg.deps}}
-	api.use('{{#meteorUser}}{{.}}:{{/meteorUser}}{{destPkgPrefix}}{{.}}@{{bootstrap.version}}', 'client');
-	{{/pkg.deps}}
-	{{#pkg.files}}
-	api.addFiles('{{{.}}}', 'client');
-	{{/pkg.files}}
+  api.versionsFrom('1.1.0.2');
+  api.use(['jquery', 'less'], 'client');
+  {{#pkg.deps}}
+  api.use('{{#meteorUser}}{{.}}:{{/meteorUser}}{{destPkgPrefix}}{{.}}@{{bootstrap.version}}', 'client');
+  {{/pkg.deps}}
+  {{#pkg.files}}
+  api.addFiles('{{{.}}}', 'client');
+  {{/pkg.files}}
 });
